0b00aa13a6c34e36a76fb7a97d66b93678a73dc8
jonathanslima <jonathan633.jl@gmail.com>
2016-08-10 18:47:28 -0300

    put js in a external file

---
+++ projetos/js/form/src/js/main.js
@@ -0,0 +1,62 @@
+// var validar = document.querySelector('#formEnviar');
+
+// validar.addEventListener('click', function(){
+// 	var nome						= form1.nome.value;
+// 	var email						= form1.email.value;
+// 	var cpf							= form1.cpf.value;
+// 	var dataNascimento	= form1.datanascimento.value;
+
+// 	if (nome === '' || !isNaN(nome)){
+// 		document.querySelector('.alerta-nome').style.display = 'block';
+// 		form1.nome.focus();
+// 		return false;
+// 	}
+
+// 	if (email === '' || email.indexOf('@') == -1 || email.indexOf('.') == -1){
+// 		document.querySelector('.alerta-email').style.display = 'block';
+// 		form1.email.focus();
+// 		return false;
+// 	}
+
+// 	if (cpf === ''){
+// 		document.querySelector('.alerta-cpf').style.display = 'block';
+// 		form1.cpf.focus();
+// 		return false;
+// 	}
+
+// 	if (dataNascimento === ''){
+// 		document.querySelector('.alerta-data').style.display = 'block';
+// 		form1.cpf.focus();
+// 		return false;
+// 	}
+// });
+function validar(){
+	var nome						= form1.nome.value;
+	var email						= form1.email.value;
+	var cpf							= form1.cpf.value;
+	var dataNascimento	= form1.datanascimento.value;
+
+	if (nome === '' || !isNaN(nome)) {
+		document.querySelector('.alerta-nome').style.display = 'block';
+		form1.nome.focus();
+		return false;
+	}
+
+	if (email === '' || email.indexOf('@') == -1 || email.indexOf('.') == -1){
+		document.querySelector('.alerta-email').style.display = 'block';
+		form1.email.focus();
+		return false;
+	}
+
+	if (cpf === ''){
+		document.querySelector('.alerta-cpf').style.display = 'block';
+		form1.cpf.focus();
+		return false;
+	}
+
+	if (dataNascimento === ''){
+		document.querySelector('.alerta-data').style.display = 'block';
+		form1.cpf.focus();
+		return false;
+	}
+}
